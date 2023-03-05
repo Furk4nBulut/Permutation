7927918bc1d60d4d5ddcc431d868988dc83d2d82
Furkan Bulut <furkanbtng@gmail.com>
2023-03-05 20:01:36 +0300

    Master branch is created. To upload clean codes.

--- src/Permutation.java
+++ src/Permutation.java
@@ -2,49 +2,36 @@ import java.util.HashSet;
 import java.util.Scanner;
 public class Permutation {
     public static void main(String[] args) {
-        /* The first test string is "ABCDEF".Its result must be 720.
-           The second test sting is "ABACBC".Its result must be 90.
-         */
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Input the string which you want to find all permutations: ");
+               Scanner scan = new Scanner(System.in);
+        System.out.print("Input to something which you want to find all permutations: ");
         String input = scan.next();
         result = generatePermutations(input);
         permutations(result);
         System.out.println("The factorial is: " + factorial());
     }
-   // public static int counter=0; //declared a public counter to use in factorial method.
-
-   public static HashSet<String> result = new HashSet<String>(); // declared a hashset list.
-                                                                  // To use in methods.
+   public static HashSet<String> result = new HashSet<String>();
     public static int factorial () {
                 return result.size();
-    } // For printing factorial.
-
-    public static HashSet<String> generatePermutations(String input) { // this method is generating variatons of permutation.
-        HashSet<String> result = new HashSet<String>(); // Ask your teacher(!)
+    }
+    public static HashSet<String> generatePermutations(String input) {
+        HashSet<String> result = new HashSet<String>();
         if (input.length() == 1) {
             result.add(input);
-            return result; //hash list created.
+            return result;
         } else {
             for (int i = 0; i < input.length(); i++) {
                 char letter = input.charAt(i);
-                 //System.out.println(letter);
                 String newText = input.substring(0, i) + input.substring(i + 1);
                 for (String addingPermutation : generatePermutations(newText)) {
                     result.add(letter + addingPermutation);
                 }
             }
-         //   counter = result.size(); // the size of list is created.
-                                    // (this mean is the number of variations are also calculated due to the hashset.)
-            return result; //hashset  list created
+            return result;
         }
     }
-    public static void permutations(HashSet<String> result){ // this method is using for print variatons without 'list'.
+    public static void permutations(HashSet<String> result){
         result.forEach(element -> System.out.println(element));
-        /*Iterator itr = result.iterator();
-        while (itr.hasNext() ){
-            System.out.println(itr.next());
-        */
+
         }
 
     }
